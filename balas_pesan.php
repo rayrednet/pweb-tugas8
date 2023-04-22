8647d14334b91abb75d3d43fcc9fd1acb5e62e56
rayssa <rayssa.sibarani@gmail.com>
2023-04-22 20:14:55 +0700

    updated ui

--- balas_pesan.php
+++ balas_pesan.php
@@ -33,23 +33,27 @@ if (mysqli_num_rows($query) < 1) {
 
         <form action="proses-reply.php" method="POST">
         <input type="hidden" name="id" value="<?php echo $hasil['id'] ?>" />
-        <div class="form-group">
-          <label for="nama">Pengirim:</label>
-          <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $hasil['nama'] ?>" readonly>
+
+        <div class="container">
+          <div class="form-group">
+            <form>
+            <label for="nama">Pengirim:</label>
+            <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $hasil['nama'] ?>" readonly>
+          </div>
+          <div class="form-group">
+            <label for="pesan">Pesan:</label>
+            <textarea class="form-control" id="pesan" name="pesan" rows="3" readonly><?php echo $hasil['pesan'] ?></textarea>
+          </div>
+          <div class="form-group">
+            <label for="balasan">Balasan:</label>
+            <textarea class="form-control" id="balasan" name="balasan" rows="3" required></textarea>
+          </div>
+          <br>
+          <button type="submit" class="btn btn-primary" name="save">Simpan</button>
+        </form>
         </div>
-        <div class="form-group">
-          <label for="pesan">Pesan:</label>
-          <textarea class="form-control" id="pesan" name="pesan" rows="3" readonly><?php echo $hasil['pesan'] ?></textarea>
-        </div>
-        <div class="form-group">
-          <label for="balasan">Balasan:</label>
-          <textarea class="form-control" id="balasan" name="balasan" rows="3" required></textarea>
-        </div>
-        <br>
-        <button type="submit" class="btn btn-primary" name="save">Simpan</button>
-      </form>
-      </div>
     </div>
+</div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
