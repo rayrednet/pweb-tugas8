9fc2186c722e654f845d9d8a14a55e6c8275ec88
rayssa <rayssa.sibarani@gmail.com>
2023-05-02 01:52:19 +0700

    updated ui for index

--- index.php
+++ index.php
@@ -7,25 +7,28 @@
         <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/index.css">
     </head>
-    <body class="bg-image">
-        <header class="header-color">  
-            <a href="#" class="logo"></a>
-            <h1>Silaturahmi Online</h1>
-        </header>
-        <div class="container-box position-absolute top-50 start-50 translate-middle">
-            <img src="assets/images/idul-fitri.png" alt="idul-fitri" width="500px">
-            <nav class="nav-main justify-content-center">
-                <a href="forum.php"> <button >User</button></a>
-                <a href="#"><button data-bs-toggle="modal" data-bs-target="#passModal">Admin</button></a>
-            </nav>
+    <body>
+        <div class= "container-fluid p-0">
+            <img src ="assets/images/index-bg.png" class="bg-image">
+            <img src = "assets/images/eri-cahyadi.png" class="overlay">
         </div>
+
+        <div class = "container-fluid p-0">
+            <img src ="assets/images/role-yay.png" class ="role">
+            <a href="forum.php"> 
+                <button class="button">User</button>
+            </a>
+            <a href="#">
+                <button class="admin" data-bs-toggle="modal" data-bs-target="#passModal">Admin</button>
+            </a>
+            </div>
         <div class="modal fade" id="passModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="ModalLabel">Admin Password</h1>
+                        <h1 class="modal-title fs-5" id="ModalLabel">Admin Verification</h1>
                     </div>
                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                     <div class="modal-body">
@@ -58,5 +61,6 @@
         }
         ?>
     </div>
+        </div>
     </body>
 </html>
